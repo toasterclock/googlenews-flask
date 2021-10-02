b63349954df327b7eed86d01824c8dd1622499cb
yisian <64526036+toasterclock@users.noreply.github.com>
2021-10-02 16:26:46 +0000

    Changed function name to fit with with standards.

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 
 Parses Google's RSS feed for news articles  
 Supports search parameters and country tags now
-#### Returns "clean-looking" url rather than google's long article url
+##### Returns "clean-looking" url rather than google's long article url
 
 ### Usage:  
 ```yourURL.com/tag/<country-code>```
 
 ```yourURL.com/search/<search parameter>```
 
+### Run it on Replit by forking my repl:
+https://replit.com/@yisiansim/googlenews-flask
--- main.py
+++ main.py
@@ -11,20 +11,20 @@ entry = []
 @app.route("/")
 def default_page():
   search_parse = feedparser.parse(f"https://news.google.com/rss?gl=sg")
-  return jsonify(searchParsed(search_parse))
+  return jsonify(search_parsed(search_parse))
 
 
 @app.route("/search/<search>")
-def search_parsingTest(search):
+def search_parsing(search):
   search_parse = feedparser.parse(f"https://news.google.com/rss/search?q={search}")
-  return jsonify(searchParsed(search_parse))
+  return jsonify(search_parsed(search_parse))
 
 @app.route('/tag/<country>')
 def search_country(country):
   search_parse = feedparser.parse(f"https://news.google.com/rss?gl={country}")
-  return jsonify(searchParsed(search_parse))
+  return jsonify(search_parsed(search_parse))
 
-def searchParsed(search_parse):
+def search_parsed(search_parse):
   entry = []
   for i in range(len(search_parse)):
     if i > 5:
